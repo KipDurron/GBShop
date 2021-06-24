e0b0e47f22142021d54fbbf1a978661159ac271d
kipdurron <ishunin97@gmail.com>
2021-06-24 08:36:59 +0300

    hw2 protocols, class

---
+++ GBShop/App/BusinessLogic/Network/RequestFactory.swift
@@ -0,0 +1,31 @@
+//
+//  RequestFactory.swift
+//  GBShop
+//
+//  Created by Илья Кадыров on 23.06.2021.
+//
+
+import Foundation
+import Alamofire
+
+class RequestFactory {
+    
+    func makeErrorParser() -> AbstractErrorParser {
+        return ErrorParser()
+    }
+    
+    lazy var commonSession: Session = {
+        let configuration = URLSessionConfiguration.default
+        configuration.httpShouldSetCookies = false
+        configuration.headers = .default
+        let manager = Session(configuration: configuration)
+        return manager
+    }()
+    
+    let sessionQueue = DispatchQueue.global(qos: .utility)
+    
+    func makeAuthRequestFatory() -> AuthRequestFactory {
+        let errorParser = makeErrorParser()
+        return Auth(errorParser: errorParser, sessionManager: commonSession, queue: sessionQueue)
+    }
+}
