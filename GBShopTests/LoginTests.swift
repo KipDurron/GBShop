4bf48744cd05d2e6c9ac3e4f256181f0c193c847
kipdurron <ishunin97@gmail.com>
2021-06-26 16:50:20 +0300

    was added tests for login and for responseCodable

---
+++ GBShopTests/LoginTests.swift
@@ -0,0 +1,38 @@
+//
+//  GBShopTests.swift
+//  GBShopTests
+//
+//  Created by Илья Кадыров on 18.06.2021.
+//
+
+import XCTest
+@testable import GBShop
+
+class LoginTests: XCTestCase {
+    
+    var requestFactory: RequestFactory!
+    let expectation = XCTestExpectation(description: "for login requests")
+
+    override func setUpWithError() throws {
+        self.requestFactory = RequestFactory()
+    }
+
+    override func tearDownWithError() throws {
+        self.requestFactory = nil
+    }
+
+    func testLogin() throws {
+        let auth = requestFactory.makeAuthRequestFatory()
+        
+        auth.login(userName: "Somebody", password: "mypassword") { response in
+            switch response.result {
+            case .success(let login):
+                self.expectation.fulfill()
+            case .failure(let error):
+                XCTFail()
+            }
+        }
+        wait(for: [expectation], timeout: 10.0)
+    }
+
+}
---
+++ GBShopTests/ResponseCodableTests.swift
@@ -0,0 +1,66 @@
+//
+//  ResponseCodableTests.swift
+//  GBShopTests
+//
+//  Created by Илья Кадыров on 26.06.2021.
+//
+
+import XCTest
+@testable import GBShop
+import Alamofire
+
+struct PostStub: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+enum ApiErrorStub: Error {
+    case fatalError
+}
+
+struct ErrorParserStub: AbstractErrorParser {
+    func parse(_ result: Error) -> Error {
+        return ApiErrorStub.fatalError
+    }
+    
+    func parse(response: HTTPURLResponse?, data: Data?, error: Error?) -> Error? {
+        return error
+    }
+}
+
+
+class ResponseCodableTests: XCTestCase {
+    
+    let expectation = XCTestExpectation(description: "Download https://jsonplaceholder.typicode.com/posts/1")
+    var errorParser: ErrorParserStub!
+    
+    override func setUpWithError() throws {
+        errorParser = ErrorParserStub()
+    }
+
+    override func tearDownWithError() throws {
+        errorParser = nil
+    }
+
+
+    func testShouldDownloadAndParse() {
+        let errorParser = ErrorParserStub()
+        
+        AF.request("https://jsonplaceholder.typicode.com/posts/1")
+            .responseCodable(errorParser: errorParser) {(response: AFDataResponse<PostStub>) in
+                switch response.result {
+                case .success(_):
+                    self.expectation.fulfill()
+                    break
+                case .failure:
+                    XCTFail()
+                }
+        }
+        wait(for: [expectation], timeout: 10.0)
+
+    }
+
+
+}
